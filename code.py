e79faed0d55ba67438088fedd10ce5eb420242b9
Daria Calitis <60710963+dbcalitis@users.noreply.github.com>
2021-10-07 13:43:46 -0400

    lesson #3

--- code.py
+++ code.py
@@ -8,20 +8,24 @@ import ugame
 import stage
 
 def game_scene():
-    # this function is the main game game_scene.
+    # this function is the main game game_scene
 
     # image banks for CircuitPython
     image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
+    image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
 
     # set the background to image 0 in the image Bank
     #   and the size (10x8 tiles of size 16x16)
     background = stage.Grid(image_bank_background, 10, 8)
 
+    # a sprite that will be updated every frame
+    ship = stage.Sprite(image_bank_sprites, 5, 75, 66)
+
     # create a stage for the background to show up on
     # and set the fram rate to 60fps
     game = stage.Stage(ugame.display, 60)
     # set the layers of all sprites, items show up in order
-    game.layers = [background]
+    game.layers = [ship] + [background]
     # render all sprites
     #   most likely you will only render the background once per game game_scene
     game.render_block()
