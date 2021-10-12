c56721e908c0393cd13bfd2796c64d8373411da9
Daria Calitis <60710963+dbcalitis@users.noreply.github.com>
2021-10-12 17:48:57 -0400

    lesson #11

--- code.py
+++ code.py
@@ -143,7 +143,9 @@ def game_scene():
                                    constants.SCREEN_X - constants.SPRITE_SIZE),
                                    constants.OFF_TOP_SCREEN)
                 break
-                                        
+
+    # for score
+    score = 0
     # image banks for CircuitPython
     image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
     image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
@@ -156,6 +158,7 @@ def game_scene():
 
     # get sound ready
     pew_sound = open("pew.wav", 'rb')
+    boom_sound = open("boom.wav", 'rb')
     sound = ugame.audio
     sound.stop()
     sound.mute(False)
@@ -289,6 +292,22 @@ def game_scene():
                           aliens[alien_number].move(constants.OFF_SCREEN_X,
                                                     constants.OFF_SCREEN_Y)
                       show_alien()
+        # each frame check if any of the lasers are touching any of the aliens
+        for laser_number in range(len(lasers)):
+            if lasers[laser_number].x > 0:
+                for alien_number in range(len(aliens)):
+                    if aliens[alien_number].x > 0:
+                        if stage.collide(lasers[laser_number].x + 6, lasers[laser_number].y + 2,
+                                         lasers[laser_number].x + 11, lasers[laser_number].y + 12,
+                                         aliens[alien_number].x + 1 , aliens[alien_number].y,
+                                         aliens[alien_number].x + 15, aliens[alien_number].y + 15):
+                            # you hit an alien
+                            aliens[alien_number].move(constants.OFF_SCREEN_X, constants.OFF_SCREEN_Y)
+                            lasers[laser_number].move(constants.OFF_SCREEN_X, constants.OFF_SCREEN_Y)
+                            sound.stop()
+                            sound.play(boom_sound)
+                            show_alien()
+                            show_alien()
         # redraw sprites
         game.render_sprites(lasers + [ship] + aliens)
         game.tick()
